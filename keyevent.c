f4a5ce6ff412b849c24688caaaa4d90fccc8fe5f
yurenchen <yurenchen@yeah.net>
2020-12-19 06:58:03 +0800

    U keyevent.c: clean unused code

--- keyevent.c
+++ keyevent.c
@@ -8,16 +8,6 @@
 #include <stdint.h>
 #include <string.h>
 
-char key[5]={0};
-char *cmd[5]={0};
-
-int parse_conf(){
-	return 0;
-}
-
-#define KEY_POWER  116
-#define KEY_RESET  117
-#define KEY_DISARM 118
 
 /*
 key is pressed, value = 1, code = 29
@@ -41,20 +31,36 @@ key is release, value = 0, code = 106
 
 int st_ctrl = 0;
 int st_alt  = 0;
+char cmd[256];
 
-int key_event(int keycode){
-	switch(keycode){
-		case KEY_RESET:
-			// system("/etc/key_reset.sh &");
-			break;	
-		case KEY_DISARM:
-			// system("/etc/key_disarm.sh &");
-			break;
-		default:
+
+int key_event(struct input_event *ev) {
+
+	switch(ev->code){
+		case KEY_CTRL:
+			st_ctrl = ev->value; break;
+		case KEY_ALT:
+			st_alt  = ev->value; break;
+		case KEY_LEFT:
+			if(st_alt & st_ctrl){
+				printf("\e[033m key-combine: ctrl+alt+left! \e[0m\n");
+				// system("xdotool key --window 0 'ctrl+alt+Left'");
+				// system("xdotool set_desktop 1");
+				// system("xdotool set_desktop --relative -- -1");
+				// system("[ \"`xdotool get_desktop`\" == 2 ] && xdotool set_desktop --relative -- -1");
+				// system("/usr/bin/test \"`xdotool get_desktop`\" == 2 && xdotool set_desktop --relative -- -1");
+				system(cmd);
+				/*
+				$ xdotool getwindowpid
+				$ xdotool getwindowname 56623117
+				win7_dev [Running] - Oracle VM VirtualBox
+				*/
+			}
 			break;
 	}
 	return 0;
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -67,12 +73,10 @@ int main(int argc, char *argv[])
 	}
 	if(argc>1) dev = argv[1];
 	if(argc>2) num = argv[2];
-	
+
 	printf("   device: %s\n", dev);
 	printf("workspace: %s\n", num);
 	printf("\n");
-
-	char cmd[256];
 	snprintf(cmd, 255, "/usr/bin/test \"`xdotool get_desktop`\" == %s && xdotool set_desktop --relative -- -1", num);
 
 
@@ -87,16 +91,12 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	//struct size
-	printf("sizeof(struct input_event): %ld\n", sizeof(struct input_event));
-	printf("sizeof(ev.time): %ld\n", sizeof(ev.time));
-	printf("sizeof(ev.type): %ld\n", sizeof(ev.type));
-	printf("sizeof(ev.code): %ld\n", sizeof(ev.code));
-	printf("sizeof(ev.value): %ld\n", sizeof(ev.value));
-	//endian
-	uint16_t a=0x1122;
-	uint8_t *x=(uint8_t *)&a;
-	uint8_t *y=x+1;
-	printf("%hx = %hhx %hhx\n", a, *x, *y);
+	// printf("sizeof(struct input_event): %ld\n", sizeof(struct input_event));
+	// printf("sizeof(ev.time): %ld\n", sizeof(ev.time));
+	// printf("sizeof(ev.type): %ld\n", sizeof(ev.type));
+	// printf("sizeof(ev.code): %ld\n", sizeof(ev.code));
+	// printf("sizeof(ev.value): %ld\n", sizeof(ev.value));
+
 
 	while(1) {
 		rb = read(fd, &ev, sizeof(struct input_event));
@@ -108,36 +108,13 @@ int main(int argc, char *argv[])
 		if (ev.type == EV_KEY) {
 			if(ev.value==2) continue; //ignore auto repeat
 
-			switch(ev.code){
-				case KEY_CTRL:
-					st_ctrl = ev.value; break;
-				case KEY_ALT:
-					st_alt  = ev.value; break;
-				case KEY_LEFT:
-					if(st_alt & st_ctrl){
-						printf("\e[033m key-combine: ctrl+alt+left! \e[0m\n");
-						// system("xdotool key --window 0 'ctrl+alt+Left'");
-						// system("xdotool set_desktop 1");
-						// system("xdotool set_desktop --relative -- -1");
-						// system("[ \"`xdotool get_desktop`\" == 2 ] && xdotool set_desktop --relative -- -1");
-						// system("/usr/bin/test \"`xdotool get_desktop`\" == 2 && xdotool set_desktop --relative -- -1");
-						system(cmd);
-/*
-getwindowpid
-$ xdotool getwindowname 56623117
-win7_dev [Running] - Oracle VM VirtualBox
-*/
-					}
-					break;
-			}
+			key_event(&ev);
 
 			if (!ev.value){
-				// printf("key is released, code = %d\n", ev.code);
 				printf("key is release, value = %d, code = %d\n", ev.value, ev.code);
 			}else{
 				printf("key is pressed, value = %d, code = %d\n", ev.value, ev.code);
 
-				// key_event(ev.code);
 			}
 		}
 	}
